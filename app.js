ca8e47974ad043ae2a3cb9acc6eb1105343d8f40
Tamilan S <tamils1809@outlook.com>
2017-09-21 19:24:13 +0530

    JavaScript ES6 Flat Arrow Function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+
+
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function() {
@@ -17,6 +20,8 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function(session) {
-    session.send("You said: %s", session.message.text);
-});
+var bot = new builder.UniversalBot(
+    connector,
+    (session) => {
+        session.send("You said: %s", session.message.text);
+    });
